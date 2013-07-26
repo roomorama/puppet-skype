d6c8481be18d684244c9bde1e21dd9ede2f3145d
Donald Piret <donald@donaldpiret.com>
2013-07-26 17:32:48 +0800

    Update init.pp
    Update Skype version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class skype {
   package { 'Skype':
     provider => 'appdmg',
-    source   => 'http://download.skype.com/macosx/Skype_6.3.0.602.dmg',
+    source   => 'http://download.skype.com/macosx/Skype_6.6.60.467.dmg',
   }
 }
